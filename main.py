c9b8078ec02322f94f441424b7577a8c87a4c385
Sankalp Pandya <137797143+Sankalp951@users.noreply.github.com>
2024-06-11 22:50:59 +0530

    Source Code For Cloud Function

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
-from flask import Flask, request, redirect, url_for, render_template
-from google.cloud import storage
-import os
+import functions_framework
+from google.cloud import bigquery
+from google.cloud.bigquery import LoadJobConfig
+import time
 
-app = Flask(__name__)
+# Triggered by a change in a storage bucket
+@functions_framework.cloud_event
+def hello_gcs(cloud_event):
+    data = cloud_event.data
 
-# Configure this environment variable with your GCS bucket name
-os.environ["GOOGLE_CLOUD_PROJECT"] = "osticket-testing-425914"
-BUCKET_NAME = 'bkt-sales-data-osticket-testing-425914'
+    event_id = cloud_event["id"]
+    event_type = cloud_event["type"]
 
-def upload_to_gcs(file, bucket_name, destination_blob_name):
-    """Uploads a file to Google Cloud Storage."""
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
-    blob.upload_from_file(file)
-    return blob.public_url
+    bucket = data["bucket"]
+    filename = data["name"]
+    metageneration = data["metageneration"]
+    timeCreated = data["timeCreated"]
+    updated = data["updated"]
 
-@app.route('/')
-def index():
-    # Check if 'url' parameter exists in the query string
-    upload_status = request.args.get('status')
-    return render_template('index.html', upload_status=upload_status)
+    print(f"Event ID: {event_id}")
+    print(f"Event type: {event_type}")
+    print(f"Bucket: {bucket}")
+    print(f"File: {filename}")
+    print(f"Metageneration: {metageneration}")
+    print(f"Created: {timeCreated}")
+    print(f"Updated: {updated}")
+    load_bq(filename)
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    if 'file' not in request.files:
-        return redirect(url_for('index', status='failure'))
-    file = request.files['file']
-    if file.filename == '':
-        return redirect(url_for('index', status='failure'))
-    if file:
-        public_url = upload_to_gcs(file, BUCKET_NAME, file.filename)
-        return redirect(url_for('index', status='success'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+dataset = 'sales'
+table = 'orders'
+def load_bq(filename):
+    client = bigquery.Client()
+    filename = filename
+    table_ref = client.dataset(dataset).table(table)
+    job_config = LoadJobConfig()
+    job_config.source_format = bigquery.SourceFormat.CSV
+    job_config.skip_leading_rows = 1
+    job_config.autodetect = True
+    uri = f'gs://bkt-sales-data-osticket-testing-425914/{filename}'
+    load_job = client.load_table_from_uri(uri, table_ref, job_config=job_config)
+    load_job.result()  
+    time.sleep(10)
+    num_rows = load_job.output_rows
+    print(f"{num_rows} rows loaded into {table}.")
